7af34872352eeb309a298d254783e44cefdf5078
Jason Dean Crossley <jasoncr@bentimagelab.com>
2019-06-22 15:18:01 -0700

    JSONImporter and CardDefinition instances

---
+++ SSJ_2019_1/Assets/Scripts/CardDefinition.cs
@@ -0,0 +1,38 @@
+﻿using SimpleJSON;
+using UnityEngine;
+
+public class CardDefinition : ScriptableObject
+{
+    public enum TYPE
+    {
+        POWER,
+        MOVEMENT,
+        UTILITY
+    }
+    public new string name { get; private set; }
+    public TYPE type { get; private set; }
+    public string description { get; private set; }
+    public string flavor { get; private set; }
+    public string assetName { get; private set; }
+
+    public void FromJSON(JSONNode json)
+    {
+        name = json["name"];
+        string type = json["type"];
+        switch (type)
+        {
+            case "P":
+                this.type = TYPE.POWER;
+                break;
+            case "M":
+                this.type = TYPE.MOVEMENT;
+                break;
+            case "U":
+                this.type = TYPE.UTILITY;
+                break;
+        }
+        description = json["description"];
+        flavor = json["flavor"];
+        assetName = json["resourceName"];
+    }
+}

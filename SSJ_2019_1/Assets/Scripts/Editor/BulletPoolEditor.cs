f9db47da04d8074def2a48c6950159f0f9f40aca
Jason Dean Crossley <jasoncr@bentimagelab.com>
2019-06-22 13:07:40 -0700

    BulletPool editor script

---
+++ SSJ_2019_1/Assets/Scripts/Editor/BulletPoolEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+
+[CustomEditor(typeof(BulletPool))]
+public class BulletPoolEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        BulletPool script = (BulletPool)target;
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.FloatField("time", script.t);
+        EditorGUILayout.FloatField("spawn time", script.spawnTime);
+        EditorGUILayout.FloatField("death time", script.deathTime);
+        EditorGUI.EndDisabledGroup();
+
+        DrawDefaultInspector();
+    }
+}
